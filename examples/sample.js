b256d497ca628906754c56d5b0cdf2c341b95c39
Jake Cyr <cyrjake@gmail.com>
2022-06-15 22:27:47 -0400

    Reformat files

--- examples/sample.js
+++ examples/sample.js
@@ -4,18 +4,18 @@ const QRUtil = require('../index');
 Main();
 
 async function Main() {
-    try {
-        // generate QR code image file
-        await QRUtil.generateImage('HELLO WORLD', 'temp.png');
+  try {
+    // generate QR code image file
+    await QRUtil.generateImage('HELLO WORLD', 'temp.png');
 
-        // read generated image file
-        const imageBuffer = fs.readFileSync('temp.png');
+    // read generated image file
+    const imageBuffer = fs.readFileSync('temp.png');
 
-        // parse the QR code image to get the contained data
-        const parsedData = await QRUtil.parseQR(imageBuffer);
+    // parse the QR code image to get the contained data
+    const parsedData = await QRUtil.parseQR(imageBuffer);
 
-        console.log(parsedData);
-    } catch (error) {
-        console.error(error);
-    }
+    console.log(parsedData);
+  } catch (error) {
+    console.error(error);
+  }
 }
--- index.js
+++ index.js
@@ -3,47 +3,47 @@ const Jimp = require('jimp');
 const QrCode = require('qrcode-reader');
 
 function generateDataURL(data) {
-    return new Promise((resolve, reject) => {
-        qrcode.toDataURL(data, (err, url) => err ? reject(err) : resolve(url));
-    });
+  return new Promise((resolve, reject) => {
+    qrcode.toDataURL(data, (err, url) => (err ? reject(err) : resolve(url)));
+  });
 }
 
 async function generateBase64(data) {
-    const dataUrl = await generateDataURL(data);
-    return dataUrl.replace(/^data:image\/png;base64,/, '');
+  const dataUrl = await generateDataURL(data);
+  return dataUrl.replace(/^data:image\/png;base64,/, '');
 }
 
 function generateImage(data, imagePath) {
-    return new Promise((resolve, reject) => {
-        qrcode.toFile(imagePath, data, (err) => err ? reject(err) : resolve());
-    });
+  return new Promise((resolve, reject) => {
+    qrcode.toFile(imagePath, data, (err) => (err ? reject(err) : resolve()));
+  });
 }
 
 function parseQR(bufferData) {
-    return new Promise((resovle, reject) => {
-        Jimp.read(bufferData, (err, image) => {
-            if (err) {
-                reject(err);
-            } else {
-                const qr = new QrCode();
+  return new Promise((resovle, reject) => {
+    Jimp.read(bufferData, (err, image) => {
+      if (err) {
+        reject(err);
+      } else {
+        const qr = new QrCode();
 
-                qr.callback = (err, value) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resovle(value.result);
-                    }
-                };
+        qr.callback = (err, value) => {
+          if (err) {
+            reject(err);
+          } else {
+            resovle(value.result);
+          }
+        };
 
-                qr.decode(image.bitmap);
-            }
-        });
+        qr.decode(image.bitmap);
+      }
     });
+  });
 }
 
 module.exports = {
-    generateDataURL,
-    generateBase64,
-    generateImage,
-    parseQR,
+  generateDataURL,
+  generateBase64,
+  generateImage,
+  parseQR,
 };
