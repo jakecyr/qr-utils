fb04c18b1481ec944f0626c2341aeaa9c45f09ab
Jake Cyr <cyrjake@gmail.com>
2023-04-09 21:32:48 -0400

    Fix audit issues

--- examples/sample.js
+++ examples/sample.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const fs = require('fs');
 const QRUtil = require('../index');
 
@@ -5,11 +6,12 @@ Main();
 
 async function Main() {
   try {
+    const IMAGE_PATH = path.join(__dirname, './temp.png');
     // generate QR code image file
-    await QRUtil.generateImage('HELLO WORLD', 'temp.png');
+    await QRUtil.generateImage('HELLO WORLD', IMAGE_PATH);
 
     // read generated image file
-    const imageBuffer = fs.readFileSync('temp.png');
+    const imageBuffer = fs.readFileSync(IMAGE_PATH);
 
     // parse the QR code image to get the contained data
     const parsedData = await QRUtil.parseQR(imageBuffer);
